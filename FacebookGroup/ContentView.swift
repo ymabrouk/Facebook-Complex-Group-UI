26549a4c4bd76a26131814d571460b2896f519b2
Yasser Mabrouk <yasser@nweave.com>
2019-10-17 01:09:40 +0200

    Initial Commit

---
+++ FacebookGroup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookGroup
+//
+//  Created by Yasser Mabrouk  on 10/17/19.
+//  Copyright © 2019 Yasser Mabrouk . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
